f2ff15b56cd7aab7a96d69773929178743158e70
Achref <achref.maarfi0@gmail.com>
2024-07-10 21:55:30 +0100

    footer responsive - header almost responsive

--- client/src/layout/Footer.jsx
+++ client/src/layout/Footer.jsx
@@ -23,73 +23,66 @@ export default function Footer() {
   ];
 
   return (
-    <footer className="absolute bottom-0 w-full p-4 flex flex-col flex-wrap pt-20 pr-16 pb-7 bg-bgColor max-md:pr-5 mt-auto ">
-      <div className="self-center w-full max-w-[1310px] max-md:max-w-full">
-        <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-          <div className="flex flex-col w-[45%] max-md:ml-0 max-md:w-full">
-            <div className="flex flex-col grow items-start text-base leading-7 text-neutral-900 max-md:mt-10">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/d53af1280a382173e4626eea1b95c7e502a15e4538a32ae75980b9d8c1707631?apiKey=bd2f05ff851942b7ba465ac3aba2ff1b&"
-                alt="Company logo"
-                className="max-w-full rounded-full aspect-square w-[103px]"
-              />
-              <div className="self-stretch mt-6">
-                L'Équilibre en Mouvement, la Pureté en Action
-              </div>
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/426a28c3bcf35cf010b72e8c3551ce7234b6cbe02571fe649248a199df2c7562?apiKey=bd2f05ff851942b7ba465ac3aba2ff1b&"
-                alt=""
-                className="mt-5 aspect-[2.56] w-[76px]"
-              />
+    <footer className="bottom-0 w-full p-4 flex justify-center items-center flex-col flex-wrap px-2 py-16 pb-7 bg-bgColor max-md:pr-5 mt-auto ">
+      <div className="flex gap-5 max-md:flex-col max-md:gap-0 self-center w-full max-w-[1310px] max-md:max-w-full max-md:ml-4">
+        <div className="flex flex-col w-[45%] max-md:ml-0 max-md:w-full">
+          <div className="flex flex-col grow items-start text-base leading-7 text-neutral-900 max-md:mt-10">
+            <img
+              loading="lazy"
+              src={require("../Images/biopilate-logo.png")}
+              alt="Company logo"
+              className="max-w-full rounded-full aspect-square w-[103px]"
+            />
+            <div className="self-stretch mt-6">
+              L'Équilibre en Mouvement, la Pureté en Action
             </div>
-          </div>
-          <div className="flex flex-col ml-5 w-[55%] max-md:ml-0 max-md:w-full">
-            <div className="mt-1.5 max-md:mt-10 max-md:max-w-full">
-              <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-                <nav className="flex flex-col w-[49%] max-md:ml-0 max-md:w-full">
-                  <div className="flex flex-wrap gap-5 text-base max-w-[257px] text-neutral-900">
-                    <div className="w-full">
-                      <h2 className="text-xl font-semibold">Menu</h2>
-                    </div>
-                    {menuItems.map((item, index) => (
-                      <div key={index} className="w-[calc(50%-10px)]">
-                        <a href="#" className="block">
-                          {item}
-                        </a>
-                      </div>
-                    ))}
-                  </div>
-                </nav>
-                <section className="flex flex-col ml-5 w-[51%] max-md:ml-0 max-md:w-full">
-                  <div className="flex flex-col grow text-base text-neutral-900 max-md:mt-10">
-                    <h2 className="text-xl font-semibold">Contact</h2>
-                    <ul className="mt-8 space-y-4">
-                      {contactInfo.map((item, index) => (
-                        <li key={index} className="flex gap-3">
-                          <item.icon
-                            className="shrink-0 w-5 h-5"
-                            aria-label={item.ariaLabel}
-                          />
-                          <div className="flex-auto">{item.text}</div>
-                        </li>
-                      ))}
-                    </ul>
-                  </div>
-                </section>
-              </div>
-            </div>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/426a28c3bcf35cf010b72e8c3551ce7234b6cbe02571fe649248a199df2c7562?apiKey=bd2f05ff851942b7ba465ac3aba2ff1b&"
+              alt=""
+              className="mt-5 aspect-[2.56] w-[76px]"
+            />
           </div>
         </div>
+        <div className="flex ml-5 w-[55%] max-md:ml-0 max-md:w-full mt-1.5 max-md:mt-10 max-md:max-w-full gap-5 max-md:flex-col max-md:gap-0">
+          <nav className="flex flex-col w-[49%] max-md:ml-0 max-md:w-full flex-wrap gap-5 text-base max-w-[257px] text-neutral-900 max-md:hidden ">
+            <div className="flex flex-wrap gap-5 text-base max-w-[257px] text-neutral-900">
+              <div className="w-full">
+                <h2 className="text-xl font-semibold">Menu</h2>
+              </div>
+              {menuItems.map((item, index) => (
+                <div key={index} className="w-[calc(50%-10px)]">
+                  <a href="#" className="block">
+                    {item}
+                  </a>
+                </div>
+              ))}
+            </div>
+          </nav>
+          <section className="flex flex-col ml-5 w-[51%] max-md:ml-5 max-md:w-full grow text-base text-neutral-900 max-md:mt-10">
+            <h2 className="text-xl font-semibold">Contact</h2>
+            <ul className="mt-8 space-y-4">
+              {contactInfo.map((item, index) => (
+                <li key={index} className="flex gap-3">
+                  <item.icon
+                    className="shrink-0 w-5 h-5"
+                    aria-label={item.ariaLabel}
+                  />
+                  <div className="flex-auto">{item.text}</div>
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
       </div>
-      <div className="flex gap-5 self-start mt-16 text-sm tracking-tight text-center capitalize text-neutral-900 max-md:flex-wrap max-md:mt-10">
-        <div className="shrink-0 my-auto max-w-full h-px border border-solid bg-neutral-900 border-neutral-900 w-[515px]" />
+      <div className="flex justify-center items-center gap-5 mt-16 text-sm tracking-tight text-center capitalize text-neutral-900 max-md:mt-10">
+        <div className="shrink-0 mt-1 max-w-full h-px bg-neutral-900 w-[630px] max-md:w-16" />
         <div className="flex-auto">
           Copyright by{" "}
           <span className="font-bold text-neutral-900">Brand & COM</span> @ 2024
           All rights reserved
         </div>
+        <div className="shrink-0 mt-1 max-w-full h-px bg-neutral-900 w-[630px] max-md:w-16" />
       </div>
     </footer>
   );
--- client/src/layout/Header.jsx
+++ client/src/layout/Header.jsx
@@ -1,5 +1,7 @@
-import React from "react";
-import { IoMdSearch } from "react-icons/io";
+import { React, useState } from "react";
+
+import { IoMdSearch, IoIosMenu } from "react-icons/io";
+import { LuUserCircle2 } from "react-icons/lu";
 
 const navItems = [
   { label: "Accueil", href: "/accueil" },
@@ -10,40 +12,88 @@ const navItems = [
   { label: "Contact", href: "/contact" },
 ];
 
-export default function Header() {
+function SearchBar() {
+  const [showSearch, setShowSearch] = useState(false);
+
   return (
-    <header className="flex gap-5 justify-between px-14 py-1.5 w-full border border-solid bg-zinc-50 border-stone-300 max-md:flex-wrap max-md:px-5 max-md:max-w-full font-lato">
-      <nav className="flex gap-5 justify-between items-center text-base text-neutral-900 max-md:flex-wrap">
-        <a href="/accueil">
-          <img
-            loading="lazy"
-            src="https://cdn.builder.io/api/v1/image/assets/TEMP/a025b06def9b74513c3c582f0417a67b1c7db878178c972a0a43b7662fe94d87?apiKey=bd2f05ff851942b7ba465ac3aba2ff1b&"
-            alt=""
-            className="shrink-0 self-stretch rounded-full aspect-square w-[81px]"
-          />
-        </a>
-        <ul className="flex gap-5 justify-between self-stretch pr-2.5 my-auto max-md:flex-wrap">
-          {navItems.map((item, index) => (
-            <li>
-              <a
-                href={item.href}
-                className="justify-center self-start py-1.5 whitespace-nowrap"
-              >
-                {item.label}
-              </a>
-            </li>
-          ))}
+    <div className="flex justify-center">
+      <button onClick={() => setShowSearch(!showSearch)}>
+        <IoMdSearch className="text-2xl text-stone-500" />
+      </button>
+      {showSearch && (
+        <div className="">
+          {/* Search Bar */}
+          <input type="text" className="" placeholder="Search..." />
+          {/* Recent Search Items */}
+          <div className="">
+            <div>Item 1</div>
+            <div>Item 2</div>
+            {/* Add more items as needed */}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <header className="flex gap-5 justify-between items-center px-14 py-1.5 w-full border border-solid bg-white border-bgColor md:flex-wrap max-md:border-none max-md:px-5 max-md:max-w-full font-lato">
+      <nav className="flex gap-5 items-center text-neutral-900 max-md:flex-wrap">
+        <ul className="flex gap-5 justify-between items-center max-md:flex-wrap">
+          <li>
+            <a href="/accueil">
+              <img
+                loading="lazy"
+                src={require("../Images/biopilate-logo.png")}
+                alt="Logo"
+                className="rounded-full w-20"
+              />
+            </a>
+          </li>
+          <ul
+            className={`flex gap-5 justify-between items-center max-md:flex-wrap ${
+              isMenuOpen ? "flex flex-nowrap" : "hidden"
+            }md:flex`}
+          >
+            {navItems.map((item, index) => (
+              <li>
+                <a
+                  href={item.href}
+                  className={`py-1.5 whitespace-nowrap ${
+                    window.location.pathname === item.href
+                      ? "custom-underline"
+                      : ""
+                  }`}
+                >
+                  {item.label}
+                </a>
+              </li>
+            ))}
+          </ul>
         </ul>
-        <IoMdSearch />
       </nav>
-      <div className="flex gap-4 my-auto font-bold text-center text-stone-500">
-        <button className="flex gap-2 my-auto text-lg">
-          <span className="justify-center my-auto">Se connecter</span>
-        </button>
-        <button className="flex flex-col justify-center text-base leading-6 rounded-lg">
-          <span className="justify-center px-10 py-4 bg-bgColor max-md:px-5">
+
+      <div className="flex justify-center items-center gap-2">
+        <SearchBar />
+        <div className="flex gap-4 pl-auto font-bold text-center text-stone-500">
+          <button className="flex items-center justify-center gap-3 my-auto text-lg">
+            <LuUserCircle2 className="text-2xl" />
+            <div className="max-md:hidden">Se connecter</div>
+          </button>
+          <button className="flex flex-col justify-center text-base leading-6 rounded-lg px-10 py-4 bg-bgColor max-md:hidden">
             Réserver
-          </span>
+          </button>
+        </div>
+
+        <button
+          className="sm:hidden" // Hide on screens larger than `md`
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {/* Icon or text to indicate menu toggle */}
+          <IoIosMenu className="text-4xl text-stone-500" />
         </button>
       </div>
     </header>
