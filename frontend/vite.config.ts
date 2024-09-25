26d009ac47654067004fbaf8da4f4f545e821218
neonara <nour.d@neonara.digital>
2024-09-25 17:08:40 +0100

    finish the buildingv3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,38 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
   plugins: [
-    react() // No need to manually specify swcOptions, Vite handles this internally.
+    react()
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), // Alias for src directory
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    target: "es2020", // Ensures modern JavaScript is used
+    target: "es2020",
+    outDir: 'dist',
+    sourcemap: true,
   },
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000', // Your backend server URL
+        changeOrigin: true, // Changes the origin of the host header to the target URL
+        rewrite: (path) => path.replace(/^\/api/, '') // Optional: rewrite path if necessary
+      }
+    }
+  },
+  define: {
+    'process.env': {
+      API_BASE_URL: 'http://141.94.23.119:8000/api/'|| 'http://localhost:8000/api/',
+    }
+  }
 });
