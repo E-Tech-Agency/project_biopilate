65f7f68124d77b7691c297eadaaa1d0022b53fb0
Nour Derwich <nourderouich159@gmail.com>
2024-12-19 15:15:40 +0100

    check

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,15 +5,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponseNotFound
-def block_root(request):
-    return HttpResponseNotFound("This page is not available.")
+
 urlpatterns = [
      path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('secret/', admin.site.urls),
     path('api/', include('accounts.urls')),
     path('api/',include('biopilate.urls')),
     path('api/', include('social_accounts.urls')),
-     path('', block_root),
+   
 ]
 
 if settings.DEBUG:
